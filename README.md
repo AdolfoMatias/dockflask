3fcf977dfcd3dd1d24f69cd9c8604e44e1f17037
Adolfo Matias <petadolfo@gmail.com>
2022-05-16 22:45:13 -0300

    ETAPA 6: Refinando documentos

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Detector de Gravidade Câncer de mama com flask e docker:  Instruções
+## Detector de Gravidade Câncer de mama com flask e docker:  Instruções
 
 ### 1 - Uso abrir sua IDE
-### 2 - Contrua a imagem: docker build -t nomedaimagem .
-### 3 - Roda a imagem: docker run -d -p 5000:5000 nomedaimagem ou docker run -p 127.0.0.1:5000:5000/tcp nomedaimagem
+### 2 - Contrua a imagem: 
+ - docker build -t nomedaimagem .
+### 3 - Roda a imagem: 
+ - Opção 1: docker run -d -p 5000:5000 nomedaimagem
+ - Opção 2: docker run -p 127.0.0.1:5000:5000/tcp nomedaimagem
 
-<footer> Desenvolvido por Adolfo Matias</footer>
+##### Desenvolvido por Adolfo Matias
--- classifier.py
+++ classifier.py
@@ -56,101 +56,101 @@ c3 = RandomForestClassifier(n_estimators=5000,random_state=42, min_samples_leaf=
 c4 = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.08, random_state=42, max_depth=4)
 clfs =  [c1,c2,c3,c4]
 
-# def criador_modelo():
-#     for clf in clfs:
-#         modelo = clf.fit(X_train, y_train)
-#         previsao = modelo.predict(X_test)
-#         acuracia= accuracy_score(y_test, previsao)
-#         precisao = precision_score(y_test, previsao)
-#         revocacao = recall_score(y_test, previsao)
-#         f1score = f1_score(y_test, previsao)
+def criador_modelo():
+    for clf in clfs:
+        modelo = clf.fit(X_train, y_train)
+        previsao = modelo.predict(X_test)
+        acuracia= accuracy_score(y_test, previsao)
+        precisao = precision_score(y_test, previsao)
+        revocacao = recall_score(y_test, previsao)
+        f1score = f1_score(y_test, previsao)
 
-#         print(f"""
-#         Modelo: {clf}
-#         Acurácia: {acuracia}
-#         Precisão: {precisao}
-#         Recall: {revocacao}
-#         F1-score: {f1score}
-#         """)
+        print(f"""
+        Modelo: {clf}
+        Acurácia: {acuracia}
+        Precisão: {precisao}
+        Recall: {revocacao}
+        F1-score: {f1score}
+        """)
 
-# criador_modelo()
+criador_modelo()
 
 
-# mlflow.set_experiment("Mamograph")
-# with mlflow.start_run():
-#     c1 = GaussianNB()
-#     c2 = DecisionTreeClassifier(min_samples_leaf=6)
-#     #estiamdor
-#     ms = 6
-#     c3 = RandomForestClassifier(n_estimators=5000,random_state=42, min_samples_leaf=6)
-#     #estimadores
-#     ne=1000
-#     mrs=6
-#     c4 = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.09, random_state=42, max_depth=4)
-#     #estimadores
-#     nxe=1000
-#     lr=0.01
-#     md=4
+mlflow.set_experiment("Mamograph")
+with mlflow.start_run():
+    c1 = GaussianNB()
+    c2 = DecisionTreeClassifier(min_samples_leaf=6)
+    #estiamdor
+    ms = 6
+    c3 = RandomForestClassifier(n_estimators=5000,random_state=42, min_samples_leaf=6)
+    #estimadores
+    ne=1000
+    mrs=6
+    c4 = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.09, random_state=42, max_depth=4)
+    #estimadores
+    nxe=1000
+    lr=0.01
+    md=4
 
-#     clfs =  [c1,c2,c3,c4]
-#     contador=0
-#     contar=0
-#     for clf in clfs:
-#         modelo = clf.fit(X_train, y_train)
-#         previsao = modelo.predict(X_test)
-#         acuracia= accuracy_score(y_test, previsao)
-#         precisao = precision_score(y_test, previsao)
-#         revocacao = recall_score(y_test, previsao)
-#         f1score = f1_score(y_test, previsao)
-#         confusao = confusion_matrix(y_test, previsao)
+    clfs =  [c1,c2,c3,c4]
+    contador=0
+    contar=0
+    for clf in clfs:
+        modelo = clf.fit(X_train, y_train)
+        previsao = modelo.predict(X_test)
+        acuracia= accuracy_score(y_test, previsao)
+        precisao = precision_score(y_test, previsao)
+        revocacao = recall_score(y_test, previsao)
+        f1score = f1_score(y_test, previsao)
+        confusao = confusion_matrix(y_test, previsao)
 
-#         print(f"""
-#         Modelo: {clf}
-#         Acurácia: {acuracia}
-#         Precisão: {precisao}
-#         Recall: {revocacao}
-#         F1-score: {f1score}
-#         """)
-#         #parametros
+        print(f"""
+        Modelo: {clf}
+        Acurácia: {acuracia}
+        Precisão: {precisao}
+        Recall: {revocacao}
+        F1-score: {f1score}
+        """)
+        #parametros
         
-#         if contador==1:
-#             mlflow.log_param("min_samples_leaf", ms)
-#         elif contador ==2:
-#             mlflow.log_param("n_estimators", ne)
-#             mlflow.log_param("min_sampels_leaf", mrs)
+        if contador==1:
+            mlflow.log_param("min_samples_leaf", ms)
+        elif contador ==2:
+            mlflow.log_param("n_estimators", ne)
+            mlflow.log_param("min_sampels_leaf", mrs)
             
-#         elif contador==3:
-#             mlflow.log_param("n_estimators", nxe)
-#             mlflow.log_param("learning_rate", lr)
-#             mlflow.log_param("max-depth", md)
-#         contador+=1
+        elif contador==3:
+            mlflow.log_param("n_estimators", nxe)
+            mlflow.log_param("learning_rate", lr)
+            mlflow.log_param("max-depth", md)
+        contador+=1
         
-#         categorias = ["benign", "malignant"]
-#         confusao = ConfusionMatrixDisplay(confusao,display_labels=categorias)
-#         confusao.plot()
-#         plt.savefig("confusao.png")
-#         mlflow.log_artifact("confusao.png")
+        categorias = ["benign", "malignant"]
+        confusao = ConfusionMatrixDisplay(confusao,display_labels=categorias)
+        confusao.plot()
+        plt.savefig("confusao.png")
+        mlflow.log_artifact("confusao.png")
 
 
-#         #metricas
-#         mlflow.log_metric("acuracia", acuracia)
-#         mlflow.log_metric("precisao", precisao)
-#         mlflow.log_metric("recall", revocacao)
-#         mlflow.log_metric("f1score", f1score)
+        #metricas
+        mlflow.log_metric("acuracia", acuracia)
+        mlflow.log_metric("precisao", precisao)
+        mlflow.log_metric("recall", revocacao)
+        mlflow.log_metric("f1score", f1score)
 
 
-#         #salvando imagens
+        #salvando imagens
        
       
-#         #modelo
+        #modelo
         
-#         cla = ["NaiveBayes", "DecisionTress", "RandomForest", "GradientBoost"]
+        cla = ["NaiveBayes", "DecisionTress", "RandomForest", "GradientBoost"]
     
-#         mlflow.sklearn.log_model(modelo,cla[contar])
-#         print("Modelo: ", mlflow.active_run().info.run_uuid)
-#         contar+=1
+        mlflow.sklearn.log_model(modelo,cla[contar])
+        print("Modelo: ", mlflow.active_run().info.run_uuid)
+        contar+=1
 
-# mlflow.end_run()
+mlflow.end_run()
 
 
 
